3b240132c18ada05f49e3004c0d675b01368e887
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-05-20 18:50:04 +0300

    Update Decrypt and Encrypt

--- RRC/Decrypt.java
+++ RRC/Decrypt.java
@@ -5,29 +5,33 @@ import java.util.Random;
 
 public class Decrypt implements Chiper {
 
-    public static HashMap<String, String> setChiper() {
+    public static HashMap<String, String> decryptChiper() {
         Random r = new Random(23);
         HashMap<String, String> alf = new HashMap<>();
-        ArrayList<String> alfLower = new ArrayList<>(Arrays.asList("abcdefghijklmnopqrstuvwxyz.,!?&/ ".split("")));
-        ArrayList<String> alfUpper = new ArrayList<>(Arrays.asList("ABCDEFGHIJKLMNOPQRSTUVWXUZ:'-@#~ ".split("")));
+        ArrayList<String> alfLower = new ArrayList<>(Arrays.asList(("abcdefghijklmnopqrstuvwxyz" +
+                ".,!?&/:'-@#~ABCDEFGHIJKLMNOPQRSTUVWXUZ 1234567890_+=<>$%^*()`").split("")));
+        ArrayList<String> alfUpper = new ArrayList<>(Arrays.asList(("abcdefghijklmnopqrstuvwxyz" +
+                ".,!?&/:'-@#~ABCDEFGHIJKLMNOPQRSTUVWXUZ 1234567890_+=<>$%^*()`").split("")));
         alfUpper.forEach((n) -> {
             int ran = r.nextInt(alfLower.size());
             alf.put(n, alfLower.get(ran));
             alfLower.remove(alfLower.get(ran));
         });
-        System.out.println(alf.toString());
+        System.out.println(alf);
         return alf;
     }
 
 
-//    public static String setReplace(String text) {
-//        StringBuilder res = new StringBuilder();
-//        String[] n = text.split("");
-//       for(int i = 0; i < text.length(); i++) {
-//            res.append(n[i].replace(n, setChiper().get()));
-//        }
-//        return res.toString();
-//    }
+    public static String decrypt(String text) {
+        StringBuilder res = new StringBuilder();
+        String[] n = text.split("");
+        for (int i = 0; i < text.length(); i++) {
+            res.append(decryptChiper().get(n[i]));
+        }
+        return res.toString();
+    }
 
-
+    public static void main(String[] args) {
+        System.out.println(Decrypt.decrypt("123"));
+    }
 }
--- RRC/Encrypt.java
+++ RRC/Encrypt.java
@@ -1,2 +1,38 @@
-public class Encrypt {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Random;
+
+public class Encrypt implements Chiper {
+
+    public static HashMap<String, String> encryptChiper() {
+
+        Random r = new Random(23);
+        HashMap<String, String> alf = new HashMap<>();
+        ArrayList<String> alfLower = new ArrayList<>(Arrays.asList(("abcdefghijklmnopqrstuvwxyz" +
+                ".,!?&/:'-@#~ABCDEFGHIJKLMNOPQRSTUVWXUZ 1234567890_+=<>$%^*()`").split("")));
+        ArrayList<String> alfUpper = new ArrayList<>(Arrays.asList(("abcdefghijklmnopqrstuvwxyz" +
+                ".,!?&/:'-@#~ABCDEFGHIJKLMNOPQRSTUVWXUZ 1234567890_+=<>$%^*()`").split("")));
+        alfUpper.forEach((n) -> {
+            int ran = r.nextInt(alfLower.size());
+            alf.put(alfLower.get(ran), n);
+            alfLower.remove(alfLower.get(ran));
+        });
+        System.out.println(alf);
+        return alf;
+    }
+
+
+    public static String encrypt(String text) {
+        StringBuilder res = new StringBuilder();
+        String[] n = text.split("");
+        for (int i = 0; i < text.length(); i++) {
+            res.append(encryptChiper().get(n[i]));
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Encrypt.encrypt("2TG"));
+    }
 }
