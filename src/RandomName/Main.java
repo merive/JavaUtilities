4c7c2dc31bbf48747b1ab9197d8afda73f97d0e6
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-05-31 13:12:51 +0300

    Add Controller and Main

---
+++ src/RandomName/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("RandomName/sample.fxml"));
+        primaryStage.setTitle("RandomName");
+        primaryStage.setScene(new Scene(root, 525, 187));
+        primaryStage.show();
+    }
+}
