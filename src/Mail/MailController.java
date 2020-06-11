d666c884930ed2da1f6154db3af9a5296083adec
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-06-11 13:16:20 +0300

    Made GUI in Mail

---
+++ src/Mail/MailController.java
@@ -0,0 +1,13 @@
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class MailController {
+    public TextField yEmail;
+    public PasswordField yPassword;
+    public TextField to;
+    public Spinner qty;
+    public TextField title;
+    public TextArea message;
+}
