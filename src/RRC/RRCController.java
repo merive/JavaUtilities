35de1d6b061962b3ae707b350fff9c6fdbfe5e08
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-06-14 13:22:56 +0300

    Update RRC

--- src/RRC/RRCController.java
+++ src/RRC/RRCController.java
@@ -1,2 +1,15 @@
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+
 public class RRCController {
+    public Spinner<Integer> key;
+    public TextArea mes, res;
+
+    public void decrypt() {
+        res.setText(Decrypt.decrypt(mes.getText(), key.getValue()));
+    }
+
+    public void encrypt() {
+        res.setText(Encrypt.encrypt(mes.getText(), key.getValue()));
+    }
 }
