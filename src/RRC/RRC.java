ba8ef349a1b44d43ac2f94b992ffcbe5fa6c7a29
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-06-14 12:56:41 +0300

    Update RRC

--- src/RRC/RRC.java
+++ src/RRC/RRC.java
@@ -1,92 +1,22 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class RRC implements ActionListener {
+import java.io.IOException;
 
-    JFrame jf;
-    JLabel rrc, et, res, key;
-    JButton encrypt, decrypt;
-    JTextArea jta, resTA;
-    JSpinner keyQty;
-
-    RRC() {
-        jf = new JFrame("RRC");
-        jf.setSize(610, 400);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        rrc = new JLabel("Random Replacement Cipher");
-        rrc.setFont(new Font("Verdana", Font.PLAIN, 16));
-        rrc.setBounds(175, 20, 236, 19);
-
-        et = new JLabel("Enter text:");
-        et.setFont(new Font("Verdana", Font.PLAIN, 16));
-        et.setBounds(10, 56, 89, 19);
-
-        res = new JLabel("Result: ");
-        res.setFont(new Font("Verdana", Font.PLAIN, 14));
-        res.setVerticalAlignment(SwingConstants.TOP);
-        res.setBounds(10, 251, 61, 19);
-
-        encrypt = new JButton("Encrypt");
-        encrypt.setFont(new Font("Verdana", Font.PLAIN, 16));
-        encrypt.setBounds(10, 176, 242, 60);
-        encrypt.addActionListener(this);
-
-        decrypt = new JButton("Decrypt");
-        decrypt.setFont(new Font("Verdana", Font.PLAIN, 16));
-        decrypt.setBounds(334, 176, 242, 60);
-        decrypt.addActionListener(this);
-        jf.getContentPane().setLayout(null);
-
-        jf.getContentPane().add(rrc);
-        jf.getContentPane().add(et);
-        jf.getContentPane().add(encrypt);
-        jf.getContentPane().add(decrypt);
-        jf.getContentPane().add(res);
-
-        jta = new JTextArea();
-        jta.setFont(new Font("Verdana", Font.PLAIN, 14));
-        jta.setColumns(4);
-        jta.setWrapStyleWord(true);
-        jta.setLineWrap(true);
-        jta.setBackground(SystemColor.menu);
-        jta.setBounds(10, 85, 566, 76);
-        jf.getContentPane().add(jta);
-
-        resTA = new JTextArea();
-        resTA.setFont(new Font("Verdana", Font.PLAIN, 14));
-        resTA.setLineWrap(true);
-        resTA.setWrapStyleWord(true);
-        resTA.setBackground(SystemColor.menu);
-        resTA.setBounds(10, 277, 566, 76);
-        jf.getContentPane().add(resTA);
-
-
-        key = new JLabel("Key:");
-        key.setFont(new Font("Verdana", Font.PLAIN, 15));
-        key.setBounds(275, 176, 45, 23);
-        jf.getContentPane().add(key);
-
-        keyQty = new JSpinner();
-        keyQty.setFont(new Font("Verdana", Font.PLAIN, 15));
-        keyQty.setModel(new SpinnerNumberModel(23, null, null, 23));
-        keyQty.setBounds(262, 206, 62, 23);
-        jf.getContentPane().add(keyQty);
-
-        jf.setVisible(true);
-    }
+public class RRC extends Application {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(RRC::new);
+        launch(args);
     }
 
-    public void actionPerformed(ActionEvent ae) {
-        if (ae.getActionCommand().equals("Decrypt")) {
-            resTA.setText(Decrypt.decrypt(jta.getText(), (Integer) keyQty.getValue()));
-        } else {
-            resTA.setText(Encrypt.encrypt(jta.getText(), (Integer) keyQty.getValue()));
-        }
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("RRC.fxml"));
+        primaryStage.setTitle("RRC");
+        primaryStage.setScene(new Scene(root, 574, 341));
+        primaryStage.show();
     }
 }
