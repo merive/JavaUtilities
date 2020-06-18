b229a653385b4df21feac175b5aab6304c3c4a1b
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-06-18 18:47:55 +0300

    Update RandomName

--- out/production/JavaUtilities/RandomName/RandomName/RandomNameController.java
+++ out/production/JavaUtilities/RandomName/RandomName/RandomNameController.java
@@ -2,6 +2,7 @@ package RandomName;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 import java.util.Random;
 
@@ -9,9 +10,16 @@ public class RandomNameController {
 
     public Label res;
     public TextField text;
+    public Pane pane;
+    public Pane startPane;
 
     public void Run() {
         res.setText("And it's: " +
                 text.getText().split(" ")[new Random().nextInt(text.getText().split(" ").length)]);
+    }
+
+    public void start() {
+        startPane.setVisible(false);
+        pane.setVisible(true);
     }
 }
