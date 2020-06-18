95b579770786b0b0c3be3fc84558d730866244c8
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-06-18 19:20:06 +0300

    Update RRC

--- src/RRC/RRC/RRCController.java
+++ src/RRC/RRC/RRCController.java
@@ -2,10 +2,13 @@ package RRC;
 
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
 
 public class RRCController {
     public Spinner<Integer> key;
     public TextArea mes, res;
+    public Pane pane;
+    public Pane startPane;
 
     public void decrypt() {
         res.setText(Decrypt.decrypt(mes.getText(), key.getValue()));
@@ -13,5 +16,10 @@ public class RRCController {
 
     public void encrypt() {
         res.setText(Encrypt.encrypt(mes.getText(), key.getValue()));
+    }
+
+    public void start() {
+        startPane.setVisible(false);
+        pane.setVisible(true);
     }
 }
